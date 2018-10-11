f7c89ba14f36d73157758fdc30470eb2d15d613d
Amanda <chang.ching@husky.neu.edu>
2018-10-10 18:20:45 -0700

    Update ReverseTheString.java

--- Assignment3/3/ReverseTheString.java
+++ Assignment3/3/ReverseTheString.java
@@ -1,5 +1,6 @@
 package Assignment3;
 
+//score 2/2
 import java.util.Scanner;
 
 public class ReverseTheString {
@@ -10,7 +11,7 @@ public class ReverseTheString {
        Scanner a=new Scanner(System.in);
        String target=a.nextLine();
        String words[]=target.trim().split("\\s++");
-       for(int i=words.length-1;i>0;i--) {
+       for(int i=words.length-1;i>0;i--) { // should be i>=0
     	   
     	   System.out.print(words[i]+" ");
        }
